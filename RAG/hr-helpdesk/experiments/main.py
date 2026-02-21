ad61f699a04d9706a5c67bcb06bb4268b2c93264
shaikkhajaibrahim <shaikkhajaibrahim@gmail.com>
2026-02-21 21:02:17 +0530

    Added experiments

--- RAG/hr-helpdesk/experiments/main.py
+++ RAG/hr-helpdesk/experiments/main.py
@@ -1,4 +1,9 @@
+import os
 def main():
+    # print the process details like pid
+    print("Started process with id: ", os.getpid())
+
+    name = input("Enter your name: ")
     print("Hello from experiments!")
 
 
